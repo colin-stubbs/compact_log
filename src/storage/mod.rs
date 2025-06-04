34f635dc9180fcc3fba6ddf45ddd1ef56933f6bc
Pierre Barre <pierre@barre.sh>
2025-06-04 10:19:37 +0200

    Increase default `max_batch_size`

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -84,7 +84,7 @@ pub struct CtStorage {
 impl BatchConfig {
     pub fn default() -> Self {
         Self {
-            max_batch_size: 100,
+            max_batch_size: 10_000,
             max_batch_timeout_ms: 500,
         }
     }
