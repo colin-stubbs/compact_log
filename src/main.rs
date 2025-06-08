62c7f0795d65bfff37cccf5c95644a6397a73044
Pierre Barre <pierre@barre.sh>
2025-06-08 13:25:47 +0200

    Reduce block size

--- src/main.rs
+++ src/main.rs
@@ -259,7 +259,7 @@ async fn initialize_storage(
 
     let mut db_options = Settings::default();
     db_options.compression_codec = Some(CompressionCodec::Zstd);
-    db_options.block_size = 128 * 1024; // 128 KB
+    db_options.block_size = 64 * 1024; // 64 KB
 
     let compactor_options: CompactorOptions = CompactorOptions {
         poll_interval: Duration::from_millis(100),
