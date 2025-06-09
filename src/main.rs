99f4f2965f11f13087dbf8d7e8fe21370071268b
Pierre Barre <pierre@barre.sh>
2025-06-09 06:21:11 +0200

    Updater slatedb settings

--- src/main.rs
+++ src/main.rs
@@ -279,6 +279,8 @@ async fn initialize_storage(
     });
 
     let compactor_options: CompactorOptions = CompactorOptions {
+        max_sst_size: 64 * 1024 * 1024, // 64 MB
+
         poll_interval: Duration::from_millis(100),
         max_concurrent_compactions: 16,
         sst_iterator_options: SstIteratorOptions {
