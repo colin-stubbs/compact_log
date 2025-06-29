e8091613a47bef7ce940304ff4041d5fc096f1d8
Pierre Barre <pierre@barre.sh>
2025-06-30 00:19:53 +0200

    Bump `MAX_ENTRIES`to 1000 in `get-entries`

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -598,7 +598,7 @@ pub async fn get_entries(
         ));
     }
 
-    const MAX_ENTRIES: u64 = 64;
+    const MAX_ENTRIES: u64 = 1000;
 
     let count = (params.end - params.start + 1).min(MAX_ENTRIES);
     let end = params.start + count - 1;
